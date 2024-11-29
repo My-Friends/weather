efc0d097e71b6fb84ecbfa3391316ec98181d8f1
abdulaziz <thugstools966@gmail.com>
2024-11-29 12:48:40 +0500

    weather_page adding

--- lib/feature/weather_page/weather_page.dart
+++ lib/feature/weather_page/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../common/colors/app_colors.dart';
 
 class WeatherPage extends StatefulWidget {
@@ -25,7 +26,61 @@ class _WeatherPageState extends State<WeatherPage> {
         ),
         child: ListView(
           children: [
-            ColoredBox(color: const Color.fromARGB(255, 179, 107, 107))
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SvgPicture.asset(
+                    "assets/icon/icon_search.svg",
+                    height: 40,
+                  ),
+                  Row(
+                    children: [
+                      SvgPicture.asset(
+                        "assets/icon/icon_circle.svg",
+                        height: 10,
+                      ),
+                      const SizedBox(width: 7),
+                      SvgPicture.asset(
+                        "assets/icon/icon_circle.svg",
+                        height: 10,
+                      ),
+                      const SizedBox(width: 7),
+                      SvgPicture.asset(
+                        "assets/icon/icon_rectangle.svg",
+                        height: 10,
+                      ),
+                      const SizedBox(width: 7),
+                      SvgPicture.asset(
+                        "assets/icon/icon_circle.svg",
+                        height: 10,
+                      ),
+                    ],
+                  ),
+                  SvgPicture.asset(
+                    "assets/icon/icon_tab.svg",
+                    height: 40,
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Stockholm,\nSweden",
+                    style: TextStyle(fontSize: 45),
+                  ),
+                  Text(
+                    "Tue, Jun 30",
+                    style: TextStyle(fontSize: 10, fontFamily: "inter_thin"),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
